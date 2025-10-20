bcdbe8a53b2badcc747b95f2c8e965bf4d280b09
UrAvgCode <dev@uravgcode.com>
2025-10-20 19:05:24 +0200

    use gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,14 @@
 plugins {
-    id("java")
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
-    id("xyz.jpenilla.run-paper") version "3.0.2"
+    alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.run.paper)
 }
 
 group = "com.uravgcode"
 version = "0.1.0"
 
-repositories {
-    mavenCentral()
-    maven {
-        name = "papermc-repo"
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
-}
-
 dependencies {
-    paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
-    compileOnly("io.github.miniplaceholders:miniplaceholders-api:3.1.0")
+    paperweight.paperDevBundle(libs.versions.paper.api)
+    compileOnly(libs.miniplaceholders)
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
 rootProject.name = "modern-essentials"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "papermc-repo"
+            url = uri("https://repo.papermc.io/repository/maven-public/")
+        }
+    }
+}
