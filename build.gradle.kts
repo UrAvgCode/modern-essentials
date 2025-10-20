a97de0ea30aa22b8770c343f82599c2b6e377417
UrAvgCode <dev@uravgcode.com>
2025-10-20 19:15:14 +0200

    add resource factory plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml.Load
+
 plugins {
     alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.resource.factory)
     alias(libs.plugins.run.paper)
 }
 
@@ -34,11 +37,20 @@ tasks.withType<JavaCompile>().configureEach {
     }
 }
 
-tasks.processResources {
-    val props = mapOf("version" to project.version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("paper-plugin.yml") {
-        expand(props)
+paperPluginYaml {
+    main = "com.uravgcode.modernessentials.ModernEssentials"
+    bootstrapper = "com.uravgcode.modernessentials.ModernEssentialsBootstrap"
+    foliaSupported = true
+    apiVersion = "1.21"
+
+    name = "modern-essentials"
+    description = "a modern essentials plugin"
+    website = "https://uravgcode.com"
+    authors.add("UrAvgCode")
+
+    dependencies.server.register("MiniPlaceholders") {
+        load = Load.BEFORE
+        required = false
+        joinClasspath = true
     }
 }
