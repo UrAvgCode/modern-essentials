7bd23255c243791ca5585a82b2623c3ba063af20
UrAvgCode <dev@uravgcode.com>
2025-10-22 02:04:35 +0200

    add static plugin instance

--- src/main/java/com/uravgcode/modernessentials/ModernEssentials.java
+++ src/main/java/com/uravgcode/modernessentials/ModernEssentials.java
@@ -2,8 +2,21 @@ package com.uravgcode.modernessentials;
 
 import com.uravgcode.modernessentials.registry.ModuleRegistrar;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class ModernEssentials extends JavaPlugin {
+    private static ModernEssentials instance = null;
+
+    public static @NotNull ModernEssentials instance() {
+        return Objects.requireNonNull(instance, "plugin not initialized");
+    }
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
     @Override
     public void onEnable() {
