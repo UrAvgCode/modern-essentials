2da36b155f473ea67966b75e85c773ca7d9f332f
UrAvgCode <dev@uravgcode.com>
2025-10-22 02:31:04 +0200

    use global region scheduler for weather commands

--- src/main/java/com/uravgcode/modernessentials/command/WeatherCommand.java
+++ src/main/java/com/uravgcode/modernessentials/command/WeatherCommand.java
@@ -2,22 +2,24 @@ package com.uravgcode.modernessentials.command;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
+import com.uravgcode.modernessentials.ModernEssentials;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.Objects;
 
 @SuppressWarnings("unused")
 public final class WeatherCommand implements BaseCommand {
 
     @Override
     public void register(@NotNull Commands registrar) {
-        var weathers = Map.<String, Command<CommandSourceStack>>of(
-            "sun", ctx -> setWeather(ctx, false, false, "commands.weather.set.clear"),
-            "rain", ctx -> setWeather(ctx, true, false, "commands.weather.set.rain"),
-            "thunder", ctx -> setWeather(ctx, true, true, "commands.weather.set.thunder")
+        final var weathers = Map.<String, Command<CommandSourceStack>>of(
+            "sun", context -> setWeather(context, false, false, "commands.weather.set.clear"),
+            "rain", context -> setWeather(context, true, false, "commands.weather.set.rain"),
+            "thunder", context -> setWeather(context, true, true, "commands.weather.set.thunder")
         );
 
         weathers.forEach((name, executor) ->
@@ -29,14 +31,17 @@ public final class WeatherCommand implements BaseCommand {
         );
     }
 
-    private static int setWeather(CommandContext<CommandSourceStack> context, boolean storm, boolean thunder, String key) {
-        var executor = context.getSource().getExecutor();
-        if (executor != null) {
-            var world = executor.getWorld();
+    private static int setWeather(CommandContext<CommandSourceStack> context, boolean storm, boolean thunder, @NotNull String key) {
+        final var executor = Objects.requireNonNull(context.getSource().getExecutor());
+        final var plugin = ModernEssentials.instance();
+        final var world = executor.getWorld();
+
+        executor.sendMessage(Component.translatable(key));
+        executor.getServer().getGlobalRegionScheduler().execute(plugin, () -> {
             world.setStorm(storm);
             world.setThundering(thunder);
-            executor.sendMessage(Component.translatable(key));
-        }
+        });
+
         return Command.SINGLE_SUCCESS;
     }
 }
